370d58d951191c1aa9737c17af2ec7fc9a291f81
Mac User <macuser@MacBook-Pro-Mac.local>
2023-05-15 12:25:11 +0200

    test commit

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,8 @@
+describe('my first test', ()=> {
+    it ('clicking "type" navigates to a new url', () => {
+        cy.visit('https://example.cypress.io')
+        cy.contains('type').click()
+        cy.url().should('include', '/commands/actions')
+
+    })
+}) 
